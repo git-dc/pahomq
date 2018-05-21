30679f832eae6ac6f5fe2b320c3092c04c6eb08f
Daniel Chirita <dschirita@gmail.com>
2018-05-22 01:04:31 +0400

    receiving from mqtt and inserting into influxdb!

--- pahomq.py
+++ pahomq.py
@@ -5,8 +5,30 @@ import datetime
 import time
 from influxdb import InfluxDBClient
 
+def stampGen(s):
+    return str(time.mktime(datetime.datetime.strptime(s,"%Y-%m-%d:%H:%M:%S").timetuple())+14400)
+
+def convert_to_influx(message):
+    junk = ["{","}",'"','ENERGY:']
+    measurements=[]
+    tags=''
+    msgDict = {}
+    
+    for thing in junk: message = message.replace(thing,"")
+    msgDec = [item.split(":") for item in message.split(',')]    
+    
+    for item in msgDec:
+        header = item.pop(0)
+        if len(item)>1:
+            item[0] = item[0].replace("T",":")
+            item=[stampGen(':'.join(item))[:-2]+"00000000"]
+            
+        msgDict[header] = item[0]
+    return msgDict
+
+        
 def on_connect(client, userdata, flags, rc):
-    print("connected with result code " + str(rc))
+    print("mosquitto connected with result code " + str(rc))
     client.subscribe("vaponic/tele/SENSOR")
 
     
@@ -15,32 +37,37 @@ def on_message(client, userdata, msg):
     #use UTC as timestamp
     receiveTime=datetime.datetime.utcnow()
     message=msg.payload.decode("utf-8")
-    isfloatValue=False
-    try:
-        #convert the string to a float so that it is stored as a number and not as a string in the db
-        val=float(message)
-        isfloatValue=True
-    except:
-        print("Could not convert " + message + " to a float value")
+    msgs=convert_to_influx(message)
+    keys = [key for key in msgs]
+    keys.remove("Time")
+    for key in keys:
         isfloatValue=False
-    if isfloatValue:
-        print(str(receiveTime) + ": " + msg.topic + " " + str(val))
-
-        json_body = [
-            {
-                "measurement":msg.topic,
-                "time":receiveTIme,
-                "fields":
+        try:
+            #convert the string to a float so that it is stored as a number and not as a string in the db
+            
+            val=float(msgs[key])
+            isfloatValue=True
+        except:
+            print("Could not convert " + message + " to a float value")
+            isfloatValue=False
+        if isfloatValue:
+            print(str(receiveTime) + ": " + key + " " + str(val))
+            json_body = [
                 {
-                    "value":val
+                    "measurement":key,
+                    #"time":msgs["Time"],
+                    "time":receiveTime,
+                    "fields":
+                    {
+                        "value":msgs[key]
+                    }
                 }
-            }
-        ]
-        dbclient.write_points(json_body)
-        print("Finished writing to InfluxDB")
+            ]
+            dbclient.write_points(json_body)
+            print("Finished writing to InfluxDB")
 
 # set up a client for influxdb
-dbclient = InfluxDBClient('localhost',8086,'admin','admin','sensordata')
+dbclient = InfluxDBClient('localhost',8086,'admin','admin','mydb')
 
 # initialize the mqtt client that should connect to the mosquitto broker
 client = mqtt.Client()
@@ -54,8 +81,7 @@ while(connOK == False):
     except:
         connOK=False
     time.sleep(2)
-
-
+    
 # blocking loop to the mosquitto broker
 client.loop_forever()
 
