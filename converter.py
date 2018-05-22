3e68db31138ed5f1070354ae39b587a81666b6cd
Daniel Chirita <dschirita@gmail.com>
2018-05-22 10:38:57 +0400

    added comments

--- converter.py
+++ converter.py
@@ -10,7 +10,8 @@ def display(myList):
     print()
 
 def stampGen(s):
-    return str(time.mktime(datetime.datetime.strptime(s,"%Y-%m-%d:%H:%M:%S").timetuple())+14400)
+    return datetime.datetime.strptime(s,"%Y-%m-%dT%H:%M:%S")
+    #return str(time.mktime(datetime.datetime.strptime(s,"%Y-%m-%dT%H:%M:%S").timetuple())+14400)
 
 def getkeys(msgs):
     keys = [key for key in msgs] 
@@ -29,13 +30,20 @@ def convert_to_influx(message):
     for item in msgDec:
         header = item.pop(0)
         if len(item)>1:
-            item[0] = item[0].replace("T",":")
-            item=[stampGen(':'.join(item))[:-2]+"000000000"]
+            #item[0] = item[0].replace("T",":")
+            #item=[stampGen(':'.join(item))[:-2]+"000000000"]
+            #print(stampGen(":".join(item)))
+            item=[stampGen(":".join(item))]
             
         msgDict[header] = item[0]
     return msgDict
         
 myMessage = '{"Time":"2018-05-16T16:02:38","ENERGY":{"Total":22.189,"Yesterday":0.790,"Today":0.529,"Period":0,"Power":44,"Factor":0.40,"Voltage":231,"Current":0.480}}'
-    
+
+s="2018-05-16T16:02:38"
+print(type(stampGen(s)))
 message = convert_to_influx(myMessage)
+print (message)
 print(getkeys(message))
+
+print(type(datetime.datetime.utcnow()))
--- pahomq.py
+++ pahomq.py
@@ -5,58 +5,65 @@ import datetime
 import time
 from influxdb import InfluxDBClient
 
-def stampGen(s):
-    return str(time.mktime(datetime.datetime.strptime(s,"%Y-%m-%d:%H:%M:%S").timetuple())+14400)
 
+# converts YYYY-MM-DD:hh:mm:ss time format to unix timestamp format
+def stampGen(s): # the +14400 is to correct for the inexplicable 4 hour lag introduced by .mktime(...)
+    return datetime.datetime.strptime(s,"%Y-%m-%dT%H:%M:%S")
+    #return str(time.mktime(datetime.datetime.strptime(s,"%Y-%m-%dT%H:%M:%S").timetuple())+14400)
+
+
+# takes a tasmota json and converts it to a dictionary 
 def convert_to_influx(message):
-    junk = ["{","}",'"','ENERGY:']
-    measurements=[]
-    tags=''
-    msgDict = {}
+    junk = ["{","}",'"','ENERGY:'] # these will be removed from the input string (json object)
+    tags='' # any measurement tags should be added here
+    msgDict = {} # the return dict
     
-    for thing in junk: message = message.replace(thing,"")
-    msgDec = [item.split(":") for item in message.split(',')]    
+    for thing in junk: message = message.replace(thing,"") # removes junk (see above) from message
+    msgDec = [item.split(":") for item in message.split(',')] # split the input string into a list of measurements
     
     for item in msgDec:
         header = item.pop(0)
         if len(item)>1:
-            item[0] = item[0].replace("T",":")
-            item=[stampGen(':'.join(item))[:-2]+"00000000"]
+
+            #the timestamp contains some ":", so it gets split into multiple entries under key "Time" in msgDec; if length of the list is more than 1 after the header is popped, it should be the timestamp entry
             
-        msgDict[header] = item[0]
+            item=[stampGen(':'.join(item))]#[:-2]]+"000000000"]
+
+            # puts in list to conform with the format of the rest of the entries
+
+        msgDict[header] = item[0] # put the header:val, where val is item[0], key-value pairs into msgDict
     return msgDict
 
-        
+# paho.mqtt.client function overload
 def on_connect(client, userdata, flags, rc):
-    print("mosquitto connected with result code " + str(rc))
+    print("connected to mosquitto broker with result code " + str(rc))
     client.subscribe("vaponic/tele/SENSOR")
+    print('subscribed to "vaponic/tele/SENSOR"')
 
-    
+# paho.mqtt.client function overload
 def on_message(client, userdata, msg):
     print("received a message on topic " + msg.topic)
     #use UTC as timestamp
     receiveTime=datetime.datetime.utcnow()
     message=msg.payload.decode("utf-8")
     msgs=convert_to_influx(message)
-    keys = [key for key in msgs]
-    keys.remove("Time")
+    keys = [key for key in msgs] # extract keys only from msgDict // ===msgs
+    keys.remove("Time") # otherwise Time will be a measurement in influx
     for key in keys:
         isfloatValue=False
-        try:
-            #convert the string to a float so that it is stored as a number and not as a string in the db
-            
+        try: # convert the value to a float so that it is stored as a number in influx and not as a string in the db
             val=float(msgs[key])
             isfloatValue=True
         except:
-            print("Could not convert " + message + " to a float value")
+            print("Could not convert " + msgs[key] + " to a float value, skipping "+key+":"+msgs[key])
             isfloatValue=False
         if isfloatValue:
-            print(str(receiveTime) + ": " + key + " " + str(val))
+            print("Received "+key+":"+str(val)+" "+str(msgs["Time"])+" at "+str(receiveTime))
             json_body = [
                 {
                     "measurement":key,
-                    #"time":msgs["Time"],
-                    "time":receiveTime,
+                    "time":msgs["Time"],
+                    #"time":receiveTime,
                     "fields":
                     {
                         "value":msgs[key]
